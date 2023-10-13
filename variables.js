65952dab7eebe25ca6d51c59ec8cc283349d39c7
Gabriel Conde <gabrielconde@Gabriels-MBP-3.lan>
2023-10-12 18:51:32 -0700

    added new exercise

---
+++ variables.js
@@ -0,0 +1,18 @@
+// What answers would I get if I ran these inputs in the
+// Javascript Console in Google Chrome.
+
+
+// I added variable "firstName" and "lastName" // so that they equal my name
+var firstName = "Gabriel";
+var lastName = "Conde";
+// I created a variable that holds the answer // of "firstName" + " " + "lastName"
+var fullName = (firstName) + " " + (lastName);
+console.log(fullName);
+// What is a + b?
+var a = 34;
+var b = 21;
+a = 2;
+a + b // a + b = 23 since a was reassigned the value of '2'
+
+// What is c equal to?
+var c; // c = undefined since it has no value assigned
