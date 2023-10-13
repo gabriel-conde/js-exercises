6aaede97724eb2006a31485ee7f297bd7bf495d6
Gabriel Conde <gabrielconde@Gabriels-MBP-3.lan>
2023-10-12 18:08:58 -0700

    completed exercise one

---
+++ exercise1.js
@@ -0,0 +1,30 @@
+// In this exercise I am practicing with data types and operators
+// What will I get if I run the following in the Javascript Console in Google Chrome?
+
+5 + "34" // 534
+5 - "4" // 1
+10 % 5 // 2
+5 % 10 // 5
+"Java" + "Script"
+" " + " "
+" " + 0
+true + true
+true + false
+false + true
+false - true
+3 - 4
+"Bob" - "bill"
+
+5 >= 1
+0 === 1
+4 <= 1
+1 != 1
+"A" > "B"
+"B" < "C"
+"a" > "A"
+"b" < "A"
+true === false
+true != true
+
+
+// Make the string: "Hi There! It's "sunny" out" by using the + sign:
