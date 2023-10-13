5357fc9b7b7bc38ca59fd8c50bdb50e8380210ab
Gabriel Conde <gabrielconde@Gabriels-MBP-3.lan>
2023-10-12 18:11:08 -0700

    updated exercise one

--- exercise1.js
+++ exercise1.js
@@ -1,30 +1,32 @@
-// In this exercise I am practicing with data types and operators
+// In this exercise I am practicing with data types and operators and the console.
+
 // What will I get if I run the following in the Javascript Console in Google Chrome?
 
-5 + "34" // 534
-5 - "4" // 1
-10 % 5 // 2
-5 % 10 // 5
-"Java" + "Script"
-" " + " "
-" " + 0
-true + true
-true + false
-false + true
-false - true
-3 - 4
-"Bob" - "bill"
+5 + "34" // '534'
+5 - "4" // '1'
+10 % 5 // '2'
+5 % 10 // '5'
+"Java" + "Script" // 'JavaScript'
+" " + " " // '  '
+" " + 0 // ' 0'
+true + true // '2'
+true + false // '1'
+false + true // '1'
+false - true // '-1'
+3 - 4 // '-1'
+"Bob" - "bill" // NaN, "Bob" and "bill" are strings not numbers
 
-5 >= 1
-0 === 1
-4 <= 1
-1 != 1
-"A" > "B"
-"B" < "C"
-"a" > "A"
-"b" < "A"
-true === false
-true != true
+5 >= 1 // true
+0 === 1 // false
+4 <= 1 // false
+1 != 1 // false
+"A" > "B" // false
+"B" < "C" // true
+"a" > "A" // true
+"b" < "A"// false
+true === false // false
+true != true // false
 
 
-// Make the string: "Hi There! It's "sunny" out" by using the + sign:
+// How can I make the string: "Hi There! It's "sunny" out." by using the + sign?
+"Hi There! " + "It's \"sunny\" out."
