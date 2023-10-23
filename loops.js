fc0649c4fe75014496fd6b11c2f6b77ba62af91c
Gabriel Conde <gabrielconde@Gabriels-MBP-3.wifi.smc.edu>
2023-10-23 11:23:04 -0700

    add loops

---
+++ loops.js
@@ -0,0 +1,22 @@
+var list = [
+    "Apple",
+    "Orange",
+    "Grape",
+    "Strawberry",
+    "Blueberry",
+    "Raspberry",
+    "Pineapple",
+    "Mango",
+    "Kiwi",
+    "Cherry",
+    "Peach",
+    "Papaya",
+    "Watermelon",
+    "Lemon",
+    "Plum",
+    "Cantaloupe",
+];
+
+for (var i=0; i < list.length; i++) {
+    console.log(list[i] + "!!");
+}
